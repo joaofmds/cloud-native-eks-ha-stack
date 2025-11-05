c05a969d10295248d9422a187abbcc2338884787
João Souza <ms.joao.felipe@gmail.com>
2025-11-05 16:36:02 -0300

    chore: update Terraform modules

--- infra/terraform/envs/dev/main.tf
+++ infra/terraform/envs/dev/main.tf
@@ -13,7 +13,7 @@ locals {
 }
 
 module "s3_loki" {
-  source = "../../s3-loki"
+  source = "../../modules/s3-loki"
 
   name_prefix = local.name_prefix
   project     = var.project
@@ -21,14 +21,14 @@ module "s3_loki" {
   owner       = var.owner
   tags        = var.tags
 
-  bucket_name = var.s3_loki_bucket_name
-  versioning  = var.s3_loki_enable_versioning
+  bucket_name   = var.s3_loki_bucket_name
+  versioning    = var.s3_loki_enable_versioning
   force_destroy = var.s3_loki_force_destroy
   kms_key_arn   = var.s3_loki_kms_key_arn
 }
 
 module "s3_tempo" {
-  source = "../../s3-tempo"
+  source = "../../modules/s3-tempo"
 
   name_prefix = local.name_prefix
   project     = var.project
@@ -36,15 +36,15 @@ module "s3_tempo" {
   owner       = var.owner
   tags        = var.tags
 
-  bucket_name  = var.s3_tempo_bucket_name
-  versioning   = var.s3_tempo_enable_versioning
-  force_destroy = var.s3_tempo_force_destroy
-  kms_key_arn   = var.s3_tempo_kms_key_arn
+  bucket_name    = var.s3_tempo_bucket_name
+  versioning     = var.s3_tempo_enable_versioning
+  force_destroy  = var.s3_tempo_force_destroy
+  kms_key_arn    = var.s3_tempo_kms_key_arn
   retention_days = var.s3_tempo_retention_days
 }
 
 module "vpc" {
-  source = "../../vpc"
+  source = "../../modules/vpc"
 
   name_prefix = local.name_prefix
   project     = var.project
@@ -52,24 +52,24 @@ module "vpc" {
   owner       = var.owner
   tags        = var.tags
 
-  cidr_block                     = var.vpc_cidr_block
-  az_count                       = var.vpc_az_count
-  subnet_newbits                 = var.vpc_subnet_newbits
-  nat_gateway_strategy           = var.vpc_nat_gateway_strategy
-  create_intra_subnets           = var.vpc_create_intra_subnets
-  create_database_subnets        = var.vpc_create_database_subnets
-  enable_ipv6                    = var.vpc_enable_ipv6
-  enable_s3_gateway_endpoint     = var.vpc_enable_s3_gateway_endpoint
+  cidr_block                       = var.vpc_cidr_block
+  az_count                         = var.vpc_az_count
+  subnet_newbits                   = var.vpc_subnet_newbits
+  nat_gateway_strategy             = var.vpc_nat_gateway_strategy
+  create_intra_subnets             = var.vpc_create_intra_subnets
+  create_database_subnets          = var.vpc_create_database_subnets
+  enable_ipv6                      = var.vpc_enable_ipv6
+  enable_s3_gateway_endpoint       = var.vpc_enable_s3_gateway_endpoint
   enable_dynamodb_gateway_endpoint = var.vpc_enable_dynamodb_gateway_endpoint
-  interface_endpoints            = var.vpc_interface_endpoints
-  flow_logs_destination_type     = var.vpc_flow_logs_destination_type
-  flow_logs_s3_arn               = var.vpc_flow_logs_s3_arn
-  flow_logs_cw_retention_days    = var.vpc_flow_logs_cw_retention_days
-  flow_logs_kms_key_arn          = var.vpc_flow_logs_kms_key_arn
+  interface_endpoints              = var.vpc_interface_endpoints
+  flow_logs_destination_type       = var.vpc_flow_logs_destination_type
+  flow_logs_s3_arn                 = var.vpc_flow_logs_s3_arn
+  flow_logs_cw_retention_days      = var.vpc_flow_logs_cw_retention_days
+  flow_logs_kms_key_arn            = var.vpc_flow_logs_kms_key_arn
 }
 
 module "eks" {
-  source = "../../eks"
+  source = "../../modules/eks"
 
   cluster_name    = var.eks_cluster_name
   cluster_version = var.eks_cluster_version
@@ -79,9 +79,11 @@ module "eks" {
   owner       = var.owner
   tags        = var.tags
 
-  vpc_id              = module.vpc.vpc_id
-  cluster_subnet_ids  = module.vpc.private_subnet_ids
-  nodegroup_subnet_ids = module.vpc.private_subnet_ids
+  vpc_id                    = module.vpc.vpc_id
+  vpc_cidr_block            = module.vpc.vpc_cidr_block
+  public_subnet_cidr_blocks = module.vpc.public_subnet_cidrs
+  cluster_subnet_ids        = module.vpc.private_subnet_ids
+  nodegroup_subnet_ids      = module.vpc.private_subnet_ids
 
   endpoint_public_access       = var.eks_endpoint_public_access
   endpoint_public_access_cidrs = var.eks_endpoint_public_access_cidrs
@@ -101,35 +103,35 @@ module "eks" {
 }
 
 module "route53" {
-  source = "../../route53"
+  source = "../../modules/route53"
 
-  zone_name = var.route53_zone_name
-  project   = var.project
+  zone_name   = var.route53_zone_name
+  project     = var.project
   environment = var.environment
   owner       = var.owner
   tags        = var.tags
 
-  create_public_zone  = var.route53_create_public_zone
+  create_public_zone      = var.route53_create_public_zone
   existing_public_zone_id = var.route53_existing_public_zone_id
-  create_private_zone = var.route53_create_private_zone
+  create_private_zone     = var.route53_create_private_zone
 
   private_zone_vpc_associations = local.private_zone_associations
 
-  enable_dnssec       = var.route53_enable_dnssec
+  enable_dnssec        = var.route53_enable_dnssec
   enable_query_logging = var.route53_enable_query_logging
 }
 
 module "iam" {
-  source = "../../iam"
+  source = "../../modules/iam"
 
   project     = var.project
   environment = var.environment
   owner       = var.owner
   tags        = var.tags
 
-  cluster_name       = module.eks.cluster_name
-  oidc_provider_arn  = module.eks.oidc_provider_arn
-  oidc_issuer_url    = module.eks.oidc_provider_url
+  cluster_name      = module.eks.cluster_name
+  oidc_provider_arn = module.eks.oidc_provider_arn
+  oidc_issuer_url   = module.eks.oidc_provider_url
 
   generic_irsa_roles = var.iam_generic_irsa_roles
 
@@ -180,10 +182,10 @@ module "iam" {
   } : null
 
   enable_ebs_csi_driver = var.iam_enable_ebs_csi_driver
-  ebs_csi_driver = {
+  ebs_csi_driver = var.iam_enable_ebs_csi_driver ? {
     namespace       = var.iam_ebs_csi_driver_namespace
     service_account = var.iam_ebs_csi_driver_service_account
-  }
+  } : null
 }
 
 resource "aws_eks_addon" "ebs_csi_driver" {
@@ -192,10 +194,10 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name = module.eks.cluster_name
   addon_name   = "aws-ebs-csi-driver"
 
-  addon_version                 = var.eks_ebs_csi_driver_version
-  resolve_conflicts_on_create   = "OVERWRITE"
-  resolve_conflicts_on_update   = "OVERWRITE"
-  service_account_role_arn      = module.iam.ebs_csi_driver_role_arn
+  addon_version               = var.eks_ebs_csi_driver_version
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  service_account_role_arn    = module.iam.ebs_csi_driver_role_arn
 
   depends_on = [module.iam]
 
